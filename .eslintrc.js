bb5a34227066a670cc2e76c853428496129007ae
unsix <UnWelt@qq.com>
2018-10-16 23:12:56 +0800

    eqeqeq off eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
+    'eqeqeq':'off',
+    
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
